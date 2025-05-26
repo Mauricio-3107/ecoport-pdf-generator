11d45d7bb065095bfdf7fca67c3db5d3b3bbb4b5
Mauri <ramirezcalustrom@gmail.com>
2025-05-26 19:31:21 -0400

    Starting to shape the header

--- generate-pdf.js
+++ generate-pdf.js
@@ -7,17 +7,20 @@ const path = require("path");
   const page = await browser.newPage();
 
   const htmlPath = path.join(__dirname, "cotizacion.html");
-  const html = fs.readFileSync(htmlPath, "utf8");
+  //   const html = fs.readFileSync(htmlPath, "utf8");
 
-  await page.setContent(html, { waitUntil: "networkidle0" });
+  const htmlFileUrl = "file://" + htmlPath;
+
+  // navigate to that file so <img src="logo.png"> will resolve correctly
+  await page.goto(htmlFileUrl, { waitUntil: "networkidle0" });
 
   await page.pdf({
-    path: "cotizacion-ecp.pdf",
+    path: "cotizacion-ecoport.pdf",
     format: "A4",
     printBackground: true,
     margin: { top: "20mm", bottom: "20mm", left: "20mm", right: "20mm" },
   });
 
   await browser.close();
-  console.log("✅ PDF generated as cotizacion-ecp.pdf");
+  console.log("✅ PDF generated as cotizacion-ecoport.pdf");
 })();
